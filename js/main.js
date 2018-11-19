03a2008e63051961a309f92b1574ae680c809e24
Sandulescu Bogdan <sandulescubogdan1995@gmail.com>
2018-11-19 19:29:21 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.spoiler').on('click', 'button' ,function(event){
+    console.log(event);
+    $(this).prev().show();
+    $(this).hide();
+});
+
+
+const $button = $('<button>Spoiler Reveal</button>')
+$('.spoiler').append($button);
+
+$('.spoiler span').hide();
